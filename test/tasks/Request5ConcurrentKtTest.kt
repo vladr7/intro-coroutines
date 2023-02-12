7a17d88f39f5097e3798a24ac8bdb5fe4b97d069
Vlad <ricean.vlad7@gmail.com>
2023-02-12 15:59:24 +0200

    Concurrent
    Next: Structured concurrency﻿

--- test/tasks/Request5ConcurrentKtTest.kt
+++ test/tasks/Request5ConcurrentKtTest.kt
@@ -14,14 +14,11 @@ class Request5ConcurrentKtTest {
         val result = loadContributorsConcurrent(MockGithubService, testRequestData)
         Assert.assertEquals("Wrong result for 'loadContributorsConcurrent'", expectedConcurrentResults.users, result)
         val totalTime = System.currentTimeMillis() - startTime
-        /*
-        // TODO: uncomment this assertion
         Assert.assertEquals(
             "The calls run concurrently, so the total virtual time should be 2200 ms: " +
                     "1000 ms for repos request plus max(1000, 1200, 800) = 1200 ms for concurrent contributors requests)",
             expectedConcurrentResults.timeFromStart, totalTime
         )
-        */
         Assert.assertTrue(
             "The calls run concurrently, so the total virtual time should be 2200 ms: " +
                     "1000 ms for repos request plus max(1000, 1200, 800) = 1200 ms for concurrent contributors requests)",
